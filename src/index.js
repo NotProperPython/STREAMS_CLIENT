32cad1df3127e065ec39ea0acd12de20cd7427d9
Muhammad Umar <muhammadumer051999@gmail.com>
2023-02-08 13:16:44 +0000

    adding redux persist to the store

--- src/index.js
+++ src/index.js
@@ -3,21 +3,36 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import App from "./components/App";
 import reducers from "./reducers";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //redux dev tools configuration
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: "persist-key",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = createStore(
-  reducers,
+  persistedReducer,
   composeEnhancers(applyMiddleware(reduxThunk))
 );
+
+const persistor = persistStore(store);
+
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
